d90b93015212c1943c9c78b551b04ce563fb0c6a
Arnaldo Mera <nardy.allstars@gmail.com>
2024-04-30 18:12:51 +0200

    now solved

--- something/src/section.jsx
+++ something/src/section.jsx
@@ -2,8 +2,8 @@ import WeirdImage from "./weird-image";
 function Section() {
     return (
         <section> 
-           <h1>IMAGINE A PLACE...</h1>
-           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus incidunt unde adipisci nulla? Illum quaerat iusto dicta alias repudiandae laudantium, corporis sit aliquid quos doloribus maiores reiciendis enim laboriosam dignissimos?</p>
+           <h1 className="the-h1">IMAGINE A PLACE...</h1>
+           <p className="paragraphs">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus incidunt unde adipisci nulla? Illum quaerat iusto dicta alias repudiandae laudantium, corporis sit aliquid quos doloribus maiores reiciendis enim laboriosam dignissimos?</p>
            <button className="button1">Download for Mac</button>
            <button className="button2">Open Discord in your browser</button>        
             <WeirdImage/>
